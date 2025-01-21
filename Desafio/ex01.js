443ca0a2e7701315aaf607c3d3c933cc7ac9ce49
Kethellyn Ribeiro <kethellynbandinha@gmail.com>
2025-01-20 23:44:00 -0300

    Add files via upload

---
+++ Desafio/ex01.js
@@ -0,0 +1,30 @@
+function convertNumberToWords(number) {
+  let url =
+    "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL";
+  const request = new XMLHttpRequest();
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "text/xml");
+  let numeroConvertido
+  request.onload = function () {
+    if (request.status === 200) {
+      let resposta = request.responseXML;
+        numeroConvertido = resposta.getElementsByTagName("m:NumberToWordsResult")[0].textContent
+      console.log(numeroConvertido);
+    } else {
+      console.log("Falha na requisição");
+    }
+  };
+
+  let data = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.dataaccess.com/webservicesserver/">
+   <soapenv:Header/>
+   <soapenv:Body>
+        <web:NumberToWords>
+            <web:ubiNum>${number}</web:ubiNum>
+        </web:NumberToWords>
+    </soapenv:Body>
+    </soapenv:Envelope>
+    `
+    request.send(data)
+  return numeroConvertido;
+}
