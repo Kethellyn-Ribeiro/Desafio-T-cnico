157030d9f92218b39f727d460c91aff8be9887bd
Kethellyn Ribeiro <kethellynbandinha@gmail.com>
2025-01-21 00:09:41 -0300

    Update ex01.js

--- Desafio/ex01.js
+++ Desafio/ex01.js
@@ -1,14 +1,15 @@
 function convertNumberToWords(number) {
-  let url =
-    "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL";
+  let url = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL";
   const request = new XMLHttpRequest();
   request.open("POST", url, true);
   request.setRequestHeader("Content-Type", "text/xml");
-  let numeroConvertido
+
+  let numeroConvertido;
+
   request.onload = function () {
     if (request.status === 200) {
       let resposta = request.responseXML;
-        numeroConvertido = resposta.getElementsByTagName("m:NumberToWordsResult")[0].textContent
+      numeroConvertido = resposta.getElementsByTagName("m:NumberToWordsResult")[0].textContent;
       console.log(numeroConvertido);
     } else {
       console.log("Falha na requisição");
@@ -17,14 +18,16 @@ function convertNumberToWords(number) {
 
   let data = `
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.dataaccess.com/webservicesserver/">
-   <soapenv:Header/>
-   <soapenv:Body>
+      <soapenv:Header/>
+      <soapenv:Body>
         <web:NumberToWords>
-            <web:ubiNum>${number}</web:ubiNum>
+          <web:ubiNum>${number}</web:ubiNum>
         </web:NumberToWords>
-    </soapenv:Body>
+      </soapenv:Body>
     </soapenv:Envelope>
-    `
-    request.send(data)
+  `;
+
+  request.send(data);
+
   return numeroConvertido;
 }
